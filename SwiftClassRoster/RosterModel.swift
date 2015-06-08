411bf8605fa4fd55330f9154590ff881349d1f98
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-06-08 13:00:02 -0700

    First commit for SwiftClassRoster HW.

---
+++ SwiftClassRoster/RosterModel.swift
@@ -0,0 +1,15 @@
+//
+//  RosterModel.swift
+//  SwiftClassRoster
+//
+//  Created by Joao Paulo Galvao Alves on 6/7/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+import Foundation
+
+class Roster {
+  
+  
+  
+}
